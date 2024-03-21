89cd0638297ec04f6ef209b76186d2257a17f559
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2024-03-21 08:14:22 -0400

    examen1

---
+++ GESTIONDEEMPLEADOS.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE GESTIONDEEMPLEADOS
+
+CREATE TABLE Persona (
+    IDPersona INT IDENTITY (1,1) NOT NULL,
+    Nombre NVARCHAR(50) NOT NULL,
+    Apellido NVARCHAR(50) NOT NULL,
+    Estado NVARCHAR(20) NOT NULL,
+    PRIMARY KEY (IDPersona)
+);
+
+CREATE TABLE Empleado (
+    IDEmpleado INT IDENTITY (1,1) NOT NULL,
+    IDPersona INT NOT NULL,
+    Puesto NVARCHAR(50) NOT NULL,
+    Salario DECIMAL(18,2) NOT NULL,
+    FechaContratacion DATE NOT NULL,
+    PRIMARY KEY (IDEmpleado),
+    FOREIGN KEY (IDPersona) REFERENCES Persona (IDPersona)
+);
+
