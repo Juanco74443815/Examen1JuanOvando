0a4c938ded63b52cdb211386dcde7db3c922a35c
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2024-03-21 10:21:17 -0400

    examen1

---
+++ PersonaInsertarVista.cs
@@ -0,0 +1,53 @@
+﻿using GestionEmpleados.Bss;
+using GestionEmpleados.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionEmpleados.Vista.PersonaVista
+{
+    public partial class PersonaInsertarVista : Form
+    {
+        public PersonaInsertarVista()
+        {
+            InitializeComponent();
+        }
+        PersonaBss bss = new PersonaBss();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = textBox1.Text;
+            persona.Apellido = textBox2.Text;
+            persona.Estado = textBox3.Text;
+
+            bss.InsertarPersonaBss(persona);
+            MessageBox.Show("Se guardo correctamente");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
